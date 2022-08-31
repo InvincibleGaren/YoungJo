31e67a8cce6e251fbe56b06ae97eb17f79c9654e
lyj <llyyjj292@gmail.com>
2022-09-01 08:57:10 +0900

    0901 morning

--- src/datas/Server.js
+++ src/datas/Server.js
@@ -1,9 +1,8 @@
 const Server = {
     // baseUrl:"http://121.145.206.143:9000/"
     // baseUrl: "http://10.10.10.112:9000/"
-    baseUrl: "http://121.145.206.143:9001/"
-    // baseUrl:"http://localhost:9000/"
+    // baseUrl: "http://121.145.206.143:9001/"
+    baseUrl:"http://localhost:9000/"
 }
 
 export default Server;
-
