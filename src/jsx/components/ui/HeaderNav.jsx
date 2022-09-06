255c2abeb4cfff3bb1e298a1dff9fadc82f56d42
Jason Ahn <42033861+beat1103@users.noreply.github.com>
2022-09-06 11:54:57 +0900

    changed file name

--- src/jsx/components/ui/HeaderNav.jsx
+++ src/jsx/components/ui/HeaderNav.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import navData from '../../../datas/HeaderNav.json'
+import navData from '../../../datas/navData.json'
 
 function HeaderNav() {
 
